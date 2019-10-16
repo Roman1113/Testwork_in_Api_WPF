ba51a7bf25fbbac2583d035a5998d3adf04aaea8
Роман Максютинський <maksutroman@rivne.itstep.org>
2019-10-16 11:58:46 +0300

    add solution

---
+++ Data/ItemTipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testwork.Data
+{
+    [Table("tblItemTipe")]
+    public class ItemTipe
+    {
+            [Key]
+            public int Id { get; set; }
+            [Required, StringLength(255)]
+            public string Name { get; set; }
+            public int Count { get; set; }
+
+        [ForeignKey("ItemName")]
+            public int ItemNameId { get; set; }
+            public virtual ItemName ItemNames { get; set; }
+    }
+}
+
